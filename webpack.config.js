70c8b92098860e68126f86441bb6e0b7e5a696b4
wolfter12 <wolfter12@ukr.net>
2020-07-31 01:19:55 +0300

    new plugin 'webpack-bundle-analyzer'; changed properties

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,74 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = process.env.NODE_ENV === "production";
+
+const plugins = () => {
+  const base = [
+    new MiniCssExtractPlugin(),
+    // new CopyWebpackPlugin(
+    //   [
+    //     {
+    //       from: path.resolve(__dirname, "src/css/font-awesome/font-awesome.min.css"),
+    //       to: path.resolve(__dirname, "dist/css"),
+    //     },
+    //     {
+    //       from: path.resolve(__dirname, "src/assets/fonts"),
+    //       to: path.resolve(__dirname, "dist/fonts"),
+    //     },
+    //   ],
+    // ),
+  ];
+
+  // if (isProd) {
+  //   base.push(new BundleAnalyzerPlugin());
+  // }
+
+  return base;
+};
 
 const conf = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "./duotexteditor/dist"),
-        filename: "main.js",
-        publicPath: "dist/"
-    },
-    devServer: {
-        overlay: true
-    },
-    devtool: 'eval-sourcemap',
-    module: {
-        rules: [
-            // {
-            //     test: /\.css$/,
-            //     use: [
-            //         'style-loader',
-            //         'css-loader'
-            //     ]
-            // },
-            {
-                test: /\.(woff|woff2|ttf|eot|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts/'
-                        }
-                    }
-                ]
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "./duotexteditor/dist"),
+    filename: "main.js",
+    publicPath: "dist/",
+  },
+  module: {
+    rules: [
+      // {
+      //     test: /\.css$/,
+      //     use: [
+      //         'style-loader',
+      //         'css-loader'
+      //     ]
+      // },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
             },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader',
-                ],
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new CopyWebpackPlugin(
-            // [
-            // {
-            //     from: path.resolve(__dirname, 'src/css/font-awesome/font-awesome.min.css'),
-            //     to: path.resolve(__dirname, 'dist/css')
-            // },
-            // {
-            //     from: path.resolve(__dirname, 'src/assets/fonts'),
-            //     to: path.resolve(__dirname, 'dist/fonts')
-            // }
-            // ,
-            // {
-            //     from: path.resolve(__dirname, 'manifest.json'),
-            //     to: path.resolve(__dirname, 'dist/duotexteditor')
-            // },
-            // {
-            //     from: path.resolve(__dirname, 'background.js'),
-            //     to: path.resolve(__dirname, 'dist/duotexteditor')
-            // },
-            // {
-            //     from: path.resolve(__dirname, 'content.js'),
-            //     to: path.resolve(__dirname, 'dist/duotexteditor')
-            // },
-            // {
-            //     from: path.resolve(__dirname, 'background.js'),
-            //     to: path.resolve(__dirname, 'dist/duotexteditor')
-            // }
-            // ]
-        )
-    ]
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: plugins(),
 
 };
 
