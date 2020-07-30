b09322060ebcb55b8fbd3a4afd4afdbd05c5a13f
wolfter12 <wolfter12@gmail.com>
2020-07-31 02:00:01 +0300

    added optimization and minifying for js and css

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,22 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const isDev = process.env.NODE_ENV === "development";
 const isProd = process.env.NODE_ENV === "production";
+
+const optimization = () => {
+  const config = {};
+  if (isProd) {
+    config.minimizer = [
+      new TerserJSPlugin({}),
+      new OptimizeCSSAssetsPlugin({}),
+    ];
+  }
+  return config;
+};
 
 const plugins = () => {
   const base = [
@@ -39,6 +52,7 @@ const conf = {
     filename: "main.js",
     publicPath: "dist/",
   },
+  optimization: optimization(),
   module: {
     rules: [
       // {
