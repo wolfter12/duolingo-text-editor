aeb0833d0c9fb6b1e09a617925864a6142c5ad0e
wolfter12 <wolfter12@gmail.com>
2020-08-06 02:45:14 +0300

    added new separate webpack configs for src/js/index.js entry point

---
+++ prod.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const optimization = () => {
+  const config = {};
+  config.minimizer = [
+    new TerserJSPlugin({}),
+    new OptimizeCSSAssetsPlugin({}),
+  ];
+  return config;
+};
+
+const plugins = () => {
+  const base = [
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin(
+      [
+        {
+          from: path.resolve(__dirname, "src/css/font-awesome/font-awesome.min.css"),
+          to: path.resolve(__dirname, "duotexteditor/css"),
+        },
+        {
+          from: path.resolve(__dirname, "src/fonts"),
+          to: path.resolve(__dirname, "duotexteditor/fonts"),
+        },
+      ],
+    ),
+  ];
+  return base;
+};
+
+const conf = {
+  context: path.resolve(__dirname, "src"),
+  mode: "production",
+  entry: path.resolve(__dirname, "src/js/index.js"),
+  output: {
+    path: path.resolve(__dirname, "./duotexteditor/dist"),
+    filename: "main.js",
+    publicPath: "dist/",
+  },
+  optimization: optimization(),
+  module: {
+    rules: [
+      // {
+      //     test: /\.css$/,
+      //     use: [
+      //         'style-loader',
+      //         'css-loader'
+      //     ]
+      // },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: plugins(),
+
+};
+
+module.exports = conf;
---
+++ start.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const plugins = () => {
+  const base = [
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin(
+      [
+        {
+          from: path.resolve(__dirname, "src/css/font-awesome/font-awesome.min.css"),
+          to: path.resolve(__dirname, "duotexteditor/css"),
+        },
+        {
+          from: path.resolve(__dirname, "src/fonts"),
+          to: path.resolve(__dirname, "duotexteditor/fonts"),
+        },
+      ],
+    ),
+  ];
+  return base;
+};
+
+const conf = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: path.resolve(__dirname, "src/js/index.js"),
+  output: {
+    path: path.resolve(__dirname, "./duotexteditor/dist"),
+    filename: "main.js",
+    publicPath: "dist/",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "example"),
+    port: 4444,
+  },
+  module: {
+    rules: [
+      // {
+      //     test: /\.css$/,
+      //     use: [
+      //         'style-loader',
+      //         'css-loader'
+      //     ]
+      // },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: plugins(),
+
+};
+
+module.exports = conf;
