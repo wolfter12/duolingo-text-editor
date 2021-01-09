bcb735c4374e47cfc2397b3bb0572d8ae8df0c36
Nazar <wolfter12@gmail.com>
2021-01-10 00:01:19 +0200

    Refactored toolbar

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,49 +1,13 @@
 import "../scss/style.scss";
 
 import clearEditorOnSubmit from "./on-submit";
-import highlight from "./highlight";
 import duolingoShortcuts from "./shortcuts";
-import newline from "./newline";
+import duolingoToolbar from "./toolbar";
 
 const EasyMDE = require("easymde_duolingo");
 
 export default function createEditor(editorOptions) {
   const target = document.querySelector(`[data-editor-id=${editorOptions.dataEditorId}]`);
-
-  const duolingoToolbar = [
-    "bold",
-    "italic",
-    "strikethrough",
-    "|",
-    {
-      name: "headers",
-      className: "fa fa-header fa-heading",
-      title: "Headers",
-      children: [
-        "heading-2",
-        "heading-3",
-        "heading-4",
-        "heading-5",
-        "heading-6",
-      ],
-    },
-    highlight,
-    "|",
-    "quote",
-    "unordered-list",
-    "ordered-list",
-    "|",
-    newline,
-    "horizontal-rule",
-    "|",
-    "link",
-    "image",
-    "|",
-    "undo",
-    "redo",
-    "|",
-    "preview",
-  ];
 
   const easyMDE = new EasyMDE({
     element: target,
